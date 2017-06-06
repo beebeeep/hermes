7d5b17516b894cce3897ea95c5a89b120fe92152
Migalin Danila <me@miga.me.uk>
2017-06-07 01:08:23 +0300

    wrk

--- hermes.py
+++ hermes.py
@@ -5,30 +5,35 @@ import copy
 import logging
 import random
 import math
+import traceback
+import argparse
 
 logging.basicConfig(level=logging.DEBUG, format="%(asctime)s [%(levelname)-4.4s] %(name)s: %(message)s")
 
 # goods from A to Z with price from 1 to 26
 PRICES = dict( (chr(x), x-64) for x in range(65, 91))
 # 10% of whole cash
-DAILY_LIMIT = 0.1
-NUM_AGENTS=10000
 
 class Agent(object):
-    def __init__(self, name, money, goods):
+    def __init__(self, name, money, goods, daily_limit):
         self.name = name
         self.money = money
         self.goods = copy.copy(goods)
+        self.daily_limit = daily_limit
         self.lock = threading.Lock()
         self._today_sells = 0
         self._today_buys = 0
 
     def _goods_cost(self):
-        return sum(prices[good]*amount for good, amount in self.goods.keys())
+        return sum(PRICES[good]*amount for good, amount in self.goods.items())
+    
+    def finish_day(self):
+        self._today_buys = 0
+        self._today_sells = 0
 
     def gen_sell_order(self, stock):
         """ Generate sell order for random amount of random good we can sell today """
-        max_cost = DAILY_LIMIT*self._goods_cost() - self._today_sells
+        max_cost = self.daily_limit*self._goods_cost() - self._today_sells
 
         possible_goods = [good for good, amount in self.goods.items() if amount > 0 and PRICES[good] <= max_cost]
         if not possible_goods:
@@ -43,13 +48,13 @@ class Agent(object):
         self._today_sells += cost
         return cost
 
-    def gen_buy_order(self):
+    def gen_buy_order(self, stock):
         """ Generate buy order for random amount of random good we can buy today """
-        available_money = DAILY_LIMIT*self.money - self._today_buys
+        available_money = self.daily_limit*self.money - self._today_buys
         possible_goods = [good for good, price in PRICES.items() if PRICES[good] <= available_money]
-        good = random.sample(possible_goods, 1)[0]
         if not possible_goods:
             return 0
+        good = random.sample(possible_goods, 1)[0]
         max_amount = int(math.floor(available_money/PRICES[good]))
 
         amount = random.sample(range(1, max_amount+1), 1)[0]
@@ -57,7 +62,7 @@ class Agent(object):
         cost = PRICES[good]*amount
         logging.debug("Agent %s puts buy order for %s of %s, total cost is %s", self.name, amount, good, cost)
         stock.buy(self, good, amount)
-        self._today_sells += cost
+        self._today_buys += cost
         return cost
 
 
@@ -67,6 +72,7 @@ class Order(object):
         self.good = good
         self.amount = amount
         self.cost = PRICES[good]*amount
+        self.filled = False
 
 
 class Stock(object):
@@ -83,8 +89,6 @@ class Stock(object):
             else:
                 self.sells[good] = [order]
 
-            self.sells.append()
-
     def buy(self, buyer, good, amount):
         with self.lock:
             order = Order(buyer, good, amount)
@@ -94,18 +98,21 @@ class Stock(object):
                 self.buys[good] = [order]
 
     def process_orders(self):
-        for good, sell_order in self.sells.items():
-            if good in self.buys:
-                for buy_order in self.buys[good]:
-                    if buy_order.amount == sell_order.amount:
-                        try:
-                            self.do_deal(seller=sell_order.agent, buyer=buy_order.agent, good=good, amount=sell_order.amount)
-                        except Exception as e:
-                            logging.error("Cannot process order: %s", e)
-                        else:
-                            self.sells[good].remove(sell_order)
-                            self.buys[good].remove(buy_order)
+        for good, sell_orders in self.sells.items():
+            for sell_order in (x for x in sell_orders if not x.filled):
+                if good in self.buys:
+                    for buy_order in (x for x in self.buys[good] if not x.filled):
+                        if buy_order.amount == sell_order.amount and buy_order.agent != sell_order.agent:
+                            try:
+                                self.do_deal(seller=sell_order.agent, buyer=buy_order.agent, good=good, amount=sell_order.amount)
+                            except Exception as e:
+                                logging.error("Cannot process order: %s, Traceback: %s", e, traceback.format_exc(e))
+                            else:
+                                logging.info("transaction ok")
+                                sell_order.filled = True
+                                buy_order.filled = True
 
+    @staticmethod
     def do_deal(seller, buyer, good, amount):
         cost = PRICES[good]*amount
         if seller.goods[good] < amount:
@@ -122,24 +129,34 @@ class Stock(object):
                 buyer.money -= cost
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='hermes')
+    parser.add_argument('-n', '--max-agents', type=int, default=1000)
+    parser.add_argument('-l', '--daily-limit', type=float, default=0.1)
+    parser.add_argument('-d', '--days', type=int, default=1)
+    args = parser.parse_args()
+
     # random set of goods for all agents
     starting_goods = dict( (chr(x), random.randint(0,10)) for x in range(65, 91))
     starting_money = 100
     agents = []
-    for x in xrange(NUM_AGENTS):
-        agents.append(Agent(str(x), starting_money, starting_goods))
+    for x in xrange(args.max_agents):
+        agents.append(Agent(str(x), starting_money, starting_goods, args.daily_limit))
 
-
-    for day in xrange(1):
+    for day in xrange(args.days):
         stock = Stock()
         logging.info("====== DAY %s ======", day)
         for agent in agents:
             while True:
-                if agent.gen_buy_order() == 0:
+                if agent.gen_buy_order(stock) == 0:
                     break
             while True:
-                if agent.gen_sell_order() == 0:
+                if agent.gen_sell_order(stock) == 0:
                     break
         stock.process_orders()
+
+        for agent in agents:
+            agent.finish_day()
+
+        logging.info("Day finished")
 
 
